5a2aba2117d6763e896d5d5edced7ae9ad689718
김관경 <69471032+vangona@users.noreply.github.com>
2022-06-24 16:59:31 +0900

    Time: 71 ms (73.77%), Space: 42.2 MB (40.96%) - LeetHub

---
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let fast = head;
+    let slow = head;
+    const answer = [];
+    
+    while (fast && fast.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+        
+    return slow;
+};
