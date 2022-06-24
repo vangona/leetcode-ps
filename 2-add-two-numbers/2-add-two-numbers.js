a50c71aadaa5252cd5100a18fe340d019aa73bd6
김관경 <69471032+vangona@users.noreply.github.com>
2022-06-24 20:16:12 +0900

    Time: 248 ms (7.27%), Space: 47.2 MB (62.62%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let answerNode = new ListNode(0);
+    let calculatedNum = 0;
+    let tenFlag = false;
+    const answerHead = answerNode;
+
+    while (l1 || l2) {
+        calculatedNum = answerNode.val + (l1 && l1.val) + (l2 && l2.val);
+        if (calculatedNum < 10) {
+            if ((l1 && l1.next) || (l2 && l2.next)) answerNode.next = new ListNode(0);         
+            answerNode.val = calculatedNum;
+        } else {
+            answerNode.next = new ListNode(1);            
+            answerNode.val = calculatedNum - 10;
+        }
+        
+        l1 = l1 && l1.next;
+        l2 = l2 && l2.next;
+        answerNode = answerNode.next;
+    }
+    
+    return answerHead;
+};
