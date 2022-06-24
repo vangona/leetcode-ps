80a03d4693a9193f97c35e73c0f21eecbd8be608
김관경 <69471032+vangona@users.noreply.github.com>
2022-06-24 17:47:23 +0900

    Time: 70 ms (76.33%), Space: 41.9 MB (70.62%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let count = 0;
+    while (num) {
+        if (num % 2 === 0) {
+            num /= 2
+        } else {
+            num--;
+        };
+        count++;
+    }
+    
+    return count;
+};
