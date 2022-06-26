9e06242bf9511e749a088062aa4f71bac6b591eb
김관경 <69471032+vangona@users.noreply.github.com>
2022-06-26 14:01:33 +0900

    Time: 148 ms (63.75%), Space: 46.4 MB (84.07%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.js
@@ -4,12 +4,30 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    const merged = nums1.concat(...nums2);
-    const halfIndex = Math.floor(merged.length / 2);
-    merged.sort((a, b) => a - b);
-    if (merged.length % 2 === 0) {
-        return (merged[halfIndex] + merged[halfIndex - 1]) / 2;
+    let merged = [];
+    const N = nums1.length + nums2.length;
+    const halfLength = Math.floor(N / 2);
+    let firstPointer = 0;
+    let secondPointer = 0;
+    
+    let evenFlag = false;
+    if (N % 2 === 0) evenFlag = true;
+    
+    while (firstPointer < nums1.length && secondPointer < nums2.length) {
+        if (nums1[firstPointer] <= nums2[secondPointer]) {
+            merged.push(nums1[firstPointer]);
+            firstPointer++;
+        } else {
+            merged.push(nums2[secondPointer]);
+            secondPointer++;            
+        }
+    }
+    
+    merged = merged.concat(nums1.slice(firstPointer), nums2.slice(secondPointer));
+    
+    if (evenFlag) {
+        return (merged[halfLength] + merged[halfLength - 1]) / 2;
     } else {
-        return merged[halfIndex];
-    }; 
+        return merged[halfLength];
+    }
 };
