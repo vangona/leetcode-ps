53da1bf8e9940b7eec3934afef2b74f72d16df3c
김관경 <69471032+vangona@users.noreply.github.com>
2022-06-27 18:43:38 +0900

    Time: 91 ms (45.86%), Space: 42 MB (72.69%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -9,25 +9,18 @@ var isValid = function(s) {
     for (let i = 0; i < N; i++) {
         switch (s[i]) {
             case ')' :
-                if (parenthesis[parenthesis.length - 1] !== '(') return false;
-                parenthesis.pop();
+                if (parenthesis.pop() !== '(') return false;
                 break;
             case '}' :
-                if (parenthesis[parenthesis.length - 1] !== '{') return false;
-                parenthesis.pop();
+                if (parenthesis.pop() !== '{') return false;
                 break;
             case ']' :
-                if (parenthesis[parenthesis.length - 1] !== '[') return false;
-                parenthesis.pop();
+                if (parenthesis.pop() !== '[') return false;
                 break;
             default :
                 parenthesis.push(s[i]);
         }
     }
     
-    if (parenthesis.length) {
-        return false
-    } else {
-        return true;
-    };
+    return !Boolean(parenthesis.length);
 };
