23d5abbe09476cb65ac16ad373c993df6bfcf565
김관경 <69471032+vangona@users.noreply.github.com>
2022-06-28 22:43:02 +0900

    Time: 156 ms (57.87%), Space: 49.5 MB (69.86%) - LeetHub

--- 155-min-stack/155-min-stack.js
+++ 155-min-stack/155-min-stack.js
@@ -1,14 +1,21 @@
 class MinStack {
     constructor() {
         this.stack = [];
+        this.minHistory = [];
+        this.length = 0;
     }
 
     push(val) {
         this.stack.push(val);
+        let min = this.minHistory[this.length - 1];
+        this.minHistory.push(min === undefined ? val : Math.min(min, val));
+        this.length++;
     }
 
     pop() {
         this.stack.pop();
+        this.minHistory.pop();
+        this.length--;
     }
 
     top() {
@@ -16,6 +23,6 @@ class MinStack {
     }
 
     getMin() {
-        return Math.min(...this.stack);
+        return this.minHistory[this.length - 1];
     }
 };
