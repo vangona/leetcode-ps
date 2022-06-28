edfe84cf91724570855d0aa65b46a0dd8a78764f
김관경 <69471032+vangona@users.noreply.github.com>
2022-06-28 19:11:41 +0900

    Time: 177 ms (5.00%), Space: 52.6 MB (7.88%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    const N = nums.length;
+    const dpArr = new Array(N).fill(0);
+    dpArr[0] = nums[0];
+    
+    for (let i = 1; i < N; i++) {
+        dpArr[i] = Math.max(nums[i], dpArr[i - 1] + nums[i]);
+    }
+    
+    return Math.max(...dpArr);
+};
