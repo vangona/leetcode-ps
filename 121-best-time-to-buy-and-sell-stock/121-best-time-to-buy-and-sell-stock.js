68237ea4a49e4266e0db239f53218804edbe0f17
김관경 <69471032+vangona@users.noreply.github.com>
2022-06-28 21:47:50 +0900

    Time: 129 ms (39.24%), Space: 51.7 MB (53.09%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,16 +3,12 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    const N = prices.length;
     let lowerLimit = prices[0];
     let profit = 0;
     
-    for (let i = 1; i < N; i++) {
-        if (profit < prices[i] - lowerLimit) {
-            profit = prices[i] - lowerLimit;
-        } else if (prices[i] < lowerLimit) {
-            lowerLimit = prices[i];
-        }
+    for (let i = 1; i < prices.length; i++) {
+        profit = Math.max(profit, prices[i] - lowerLimit);
+        lowerLimit = Math.min(lowerLimit, prices[i]);
     }
     
     return profit;
