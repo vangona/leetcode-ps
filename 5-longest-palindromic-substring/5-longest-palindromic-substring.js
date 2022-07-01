7479a8134e14e5478bb7974296f79e63c911311a
김관경 <69471032+vangona@users.noreply.github.com>
2022-07-01 18:16:44 +0900

    Time: 1104 ms (20.86%), Space: 49.4 MB (27.16%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -5,13 +5,15 @@
 var longestPalindrome = function(s) {
     const N = s.length;
     const dp = Array.from({length: N}, (_, index) => s[index]);
+    let max = 1;
     
     for (let i = 1; i < N; i++) {
-        for (let j = i - dp[i - 1].length + 1; j >= 0 ; j--) {
+        for (let j = i - max + 1; j >= 0 ; j--) {
             const substring = s.slice(j, i + 1);
-            if (isPalindrom(substring) && substring.length > dp[i - 1].length) {
+            if (isPalindrom(substring) && substring.length > max) {
                 // palindrom이고, max 값이라면 dp 값을 갱신
                 dp[i] = substring;    
+                max = substring.length;
                 break;
             } else {
                 // palindrom이 아니거나 max 값보다 작다면 dp 값을 유지함.
