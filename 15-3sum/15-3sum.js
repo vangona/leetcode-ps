796af01ba0acd0d0b81a247c0745e1d83cc36171
김관경 <69471032+vangona@users.noreply.github.com>
2022-07-01 22:38:26 +0900

    Time: 275 ms (31.96%), Space: 52.4 MB (76.06%) - LeetHub

---
+++ 15-3sum/15-3sum.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    if (nums.length < 3) return [];
+    let answer = [];
+    nums.sort((a, b) => a - b);
+    
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i - 1] === nums[i]) continue;
+        let j = i + 1;
+        let k = nums.length - 1;
+        while (j < k) {
+            const sum = nums[i] + nums[j] + nums[k];
+            
+            if (sum === 0) {
+                answer.push([nums[i], nums[j], nums[k]]);
+            }
+            
+            if (sum < 0) {
+                do {
+                    j++;
+                }
+                while (nums[j] === nums[j - 1]) ;
+            } else {
+                do {
+                    k--;
+                }
+                while (nums[k] === nums[k + 1]) ;
+            }
+        }
+    }
+    
+    return answer;
+};
