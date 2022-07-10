4404eeb4601f893aa8b54deead744427210b1abb
김관경 <69471032+vangona@users.noreply.github.com>
2022-07-10 22:46:27 +0900

    Time: 225 ms (30.77%), Space: 45 MB (23.55%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    const N = nums.length;
+    const dp = new Array(N).fill(N + 1);
+    dp[0] = 0;
+    
+    for (let i = 0; i < N; i++) {
+        for (let j = 1; j <= nums[i] && i + j < N; j++) {
+            dp[i + j] = Math.min(dp[i + j], dp[i] + 1);
+        }
+    }
+    
+    return dp[N - 1];
+};
