a5e67dffc31ccf37478ac5908edaddfbf081b365
김관경 <69471032+vangona@users.noreply.github.com>
2022-07-14 16:24:46 +0900

    Time: 179 ms (67.81%), Space: 52.5 MB (84.90%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -4,43 +4,20 @@
  */
 var groupAnagrams = function(strs) {
     const result = [];
-    const indexHash = {};
-    let index = 0;
+    const indexMap = new Map();
+    let indexCount = 0;
     
     for (let i = 0; i < strs.length; i++) {
-        const sortedStr = strs[i].split('').sort();
-        let currPosition = indexHash;
+        const sortedStr = strs[i].split('').sort().join();
         
-        for (let j = 0; j < sortedStr.length - 1; j++) {
-            if (!currPosition[sortedStr[j]]) {
-                currPosition[sortedStr[j]] = {
-                    index: undefined, 
-                    hash: {},
-                };
-            }
-            
-            currPosition = currPosition[sortedStr[j]].hash;
-        }
-        
-        if (currPosition[sortedStr[sortedStr.length - 1]]) {
-            if (typeof currPosition[sortedStr[sortedStr.length - 1]]["index"] === 'number') {
-                index = currPosition[sortedStr[sortedStr.length - 1]]["index"]; 
-            } else {
-                currPosition[sortedStr[sortedStr.length - 1]].index = result.length;  
-                index = result.length;
-                result.push([]);
-            }
+        if (indexMap.has(sortedStr)) {
+            result[indexMap.get(sortedStr)].push(strs[i]);
         } else {
-            currPosition[sortedStr[sortedStr.length - 1]] = {
-                index: result.length,
-                hash: {},
-            }
+            indexMap.set(sortedStr, indexCount);
+            indexCount++;
             
-            index = result.length;
-            result.push([]);
+            result.push([strs[i]]);
         }
-        
-        result[index].push(strs[i]);
     }
     
     return result;
