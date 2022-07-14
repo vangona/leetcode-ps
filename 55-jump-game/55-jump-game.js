992359227fc4381ad6c10e4b311ec3a851e0d898
김관경 <69471032+vangona@users.noreply.github.com>
2022-07-14 16:55:52 +0900

    Time: 102 ms (72.57%), Space: 46.5 MB (58.02%) - LeetHub

--- 55-jump-game/55-jump-game.js
+++ 55-jump-game/55-jump-game.js
@@ -7,9 +7,14 @@ var canJump = function(nums) {
     let jumpForce = 0;
     
     for (let i = 0; i < nums.length; i++) {
+        // 현 지점까지 제일 멀리 나갈 수 있는 점프를 갱신해준다.
         farthestJump = Math.max(farthestJump, i + nums[i]);
         
-        // 점프가 끝났다면, 점프할 수 있는지 확인해준다.
+        // if 점프가 끝났다면,
+        // if 최대길이 점프가 현 지점을 넘는지 확인해준다.
+        // 현 지점을 넘는다면 점프력을 최대 길이 점프로 갱신해준다.
+        // 현 지점을 넘지 못한다면, 현 지점이 마지막 지점인지 확인한다.
+        // 마지막 지점이 아니라면, false를 반환한다.
         if (jumpForce === i) {
             if (farthestJump === i && i !== nums.length - 1) return false;
             
@@ -17,5 +22,6 @@ var canJump = function(nums) {
         }
     }
     
+    // 반복문이 종료되었다면, true를 반환한다.
     return true;
 };
