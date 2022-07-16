2cdca6d929a6965d2046ca9e6823490fe9367e69
김관경 <69471032+vangona@users.noreply.github.com>
2022-07-16 19:29:49 +0900

    Time: 102 ms (31.36%), Space: 42.4 MB (49.57%) - LeetHub

--- 62-unique-paths/62-unique-paths.js
+++ 62-unique-paths/62-unique-paths.js
@@ -5,6 +5,10 @@
  */
 const uniquePaths = (m, n) => {
     const dp = Array.from({length: m}, () => Array.from({length: n}, () => 1));
+    
+    for (let i = 1; i < n; i++) {
+        dp[0][i] = dp[0][i - 1];
+    }
  
     // x 좌표 방문
     for (let i = 0; i < n; i++) {
@@ -12,8 +16,6 @@ const uniquePaths = (m, n) => {
         for (let j = 1; j < m; j++) {
             if (j > 0 && i > 0) {
                 dp[j][i] = dp[j - 1][i] + dp[j][i - 1];            
-            } else if (i === 0) {
-                dp[j][i] = dp[j - 1][i];
             }
         } 
     }
