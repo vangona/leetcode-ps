33c6dd0c53df86f819f380c6763a1be1fce0edd3
김관경 <69471032+vangona@users.noreply.github.com>
2023-01-10 21:40:25 +0900

    Time: 126 ms (92.03%), Space: 50.6 MB (63.29%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.js
+++ 0707-design-linked-list/0707-design-linked-list.js
@@ -1,110 +1,114 @@
-const Node = function (val) {
-  this.val = val;
-  this.next = null;
-};
-
-const MyLinkedList = function () {
-  this.head = null;
-  this.tail = null;
-  this.size = 0;
-};
-
-MyLinkedList.prototype.getNode = function (index) {
-  let currNode = this.head;
-  let currIndex = 0;
-  while (currIndex < index) {
-    currIndex++;
-    currNode = currNode.next;
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
   }
-  return currNode;
-};
+}
 
-MyLinkedList.prototype.get = function (index) {
-  if (index < 0 || index >= this.size) return -1;
-  return this.getNode(index).val;
-};
-
-MyLinkedList.prototype.initNode = function (node) {
-  this.head = node;
-  this.tail = node;
-  this.size = 1;
-};
-
-MyLinkedList.prototype.addAtHead = function (val) {
-  const newNode = new Node(val);
-
-  if (!this.head) {
-    this.initNode(newNode);
-    return;
+class MyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
   }
 
-  const prevHead = this.head;
-  this.head = newNode;
-  this.head.next = prevHead;
-  this.size++;
-};
-
-MyLinkedList.prototype.addAtTail = function (val) {
-  const newNode = new Node(val);
-
-  if (!this.tail) {
-    this.initNode(newNode);
-    return;
+  getNode(index) {
+    let currNode = this.head;
+    let currIndex = 0;
+    while (currIndex < index) {
+      currIndex++;
+      currNode = currNode.next;
+    }
+    return currNode;
   }
 
-  this.tail.next = newNode;
-  this.tail = newNode;
-  this.size++;
-};
-
-MyLinkedList.prototype.addAtIndex = function (index, val) {
-  if (index < 0 || index > this.size) return;
-
-  const newNode = new Node(val);
-
-  if (!this.head) {
-    this.initNode(newNode);
-    return;
+  get(index) {
+    if (index < 0 || index >= this.size) return -1;
+    return this.getNode(index).val;
   }
 
-  if (index === 0) {
-    this.addAtHead(val);
-    return;
+  initNode(node) {
+    this.head = node;
+    this.tail = node;
+    this.size = 1;
   }
 
-  if (index === this.size) {
-    this.addAtTail(val);
-    return;
+  addAtHead(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.initNode(newNode);
+      return;
+    }
+
+    const prevHead = this.head;
+    this.head = newNode;
+    this.head.next = prevHead;
+    this.size++;
   }
 
-  const prevNode = this.getNode(index - 1);
-  const indexNode = prevNode.next;
+  addAtTail(val) {
+    const newNode = new Node(val);
 
-  prevNode.next = newNode;
-  newNode.next = indexNode;
-  this.size++;
-};
+    if (!this.tail) {
+      this.initNode(newNode);
+      return;
+    }
 
-MyLinkedList.prototype.deleteAtIndex = function (index) {
-  if (index < 0 || index >= this.size) return;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.size++;
+  }
 
-  if (index === 0) {
-    this.head = this.head.next;
+  addAtIndex(index, val) {
+    if (index < 0 || index > this.size) return;
+
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.initNode(newNode);
+      return;
+    }
+
+    if (index === 0) {
+      this.addAtHead(val);
+      return;
+    }
+
+    if (index === this.size) {
+      this.addAtTail(val);
+      return;
+    }
+
+    const prevNode = this.getNode(index - 1);
+    const indexNode = prevNode.next;
+
+    prevNode.next = newNode;
+    newNode.next = indexNode;
+    this.size++;
+  }
+
+  deleteAtIndex(index) {
+    if (index < 0 || index >= this.size) return;
+
+    if (index === 0) {
+      this.head = this.head.next;
+      this.size--;
+      return;
+    }
+
+    const prevNode = this.getNode(index - 1);
+
+    if (index === this.size - 1) {
+      this.tail = prevNode;
+      this.size--;
+      return;
+    }
+
+    const indexNode = prevNode.next;
+    const nextNode = indexNode.next;
+
+    prevNode.next = nextNode;
     this.size--;
-    return;
   }
-
-  const prevNode = this.getNode(index - 1);
-
-  if (index === this.size - 1) {
-    this.tail = prevNode;
-    this.size--;
-    return;
-  }
-
-  const indexNode = prevNode.next;
-  const nextNode = indexNode.next;
-
-  prevNode.next = nextNode;
-  this.size--;
-};
+}
