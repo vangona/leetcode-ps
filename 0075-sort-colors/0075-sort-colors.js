b1aa122a5bf7869ccf72460eecb75b9f45143a87
김관경 <69471032+vangona@users.noreply.github.com>
2023-02-04 00:03:11 +0900

    Time: 60 ms (86.95%), Space: 42.1 MB (56.70%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,23 +3,27 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 const sortColors = (nums) => {
-  const countArr = [0, 0, 0];
+  let redCount = 0;
+  let whiteCount = 0;
+  let blueCount = 0;
   
   nums.forEach(num => {
-    countArr[num]++;
+    if(num === 0) redCount++;
+    if(num === 1) whiteCount++;
+    if(num === 2) blueCount++;
   });
     
-  nums.forEach((_, index, arr) => {
-    if(index < countArr[0]) {
-      arr[index] = 0;
-      return;
-    } 
-    
-    if(index < countArr[0] + countArr[1]) {
-      arr[index] = 1;
+  nums.forEach((_, index) => {
+    if (redCount-- > 0) {
+      nums[index] = 0;
       return;
     }
     
-    arr[index] = 2;    
+    if (whiteCount-- > 0) {
+      nums[index] = 1;
+      return;
+    }
+    
+    if (blueCount-- > 0) nums[index] = 2;
   });
 };
