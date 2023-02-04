93891d77288bc092328ca7bfb89e1f0d0c0c4321
김관경 <69471032+vangona@users.noreply.github.com>
2023-02-05 02:56:41 +0900

    Time: 4136 ms (8.58%), Space: 48.2 MB (38.75%) - LeetHub

--- 0079-word-search/0079-word-search.js
+++ 0079-word-search/0079-word-search.js
@@ -36,7 +36,7 @@ const exist = (board, word) => {
   
   for(let i = 0; i < m; i++) {
     for(let j = 0; j < n; j++) {
-      if (answer) break;
+      if (answer) return answer;
       visited[i][j] = true;
       _backtrack(board[i][j], j, i);
       visited[i][j] = false;
