0f61b708b5e1f3af3fd895a8904b11025efc5736
김관경 <69471032+vangona@users.noreply.github.com>
2023-02-14 16:44:45 +0900

    Time: 244 ms (38.29%), Space: 47.6 MB (40.77%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -9,25 +9,29 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-const mergeKLists = (lists) => {
-  const merge = (left, right) => {
-    const resultHead = new ListNode(0);
-    let currHead = resultHead;
-    while (left && right) {
-      if (left.val < right.val) {
-        currHead.next = left;
-        left = left.next;
-      } else {
-        currHead.next = right;
-        right = right.next;
-      }
-      
-      currHead = currHead.next;
+const merge = (left, right) => {
+  const resultHead = new ListNode(0);
+  let currHead = resultHead;
+  while (left && right) {
+    if (left.val < right.val) {
+      currHead.next = left;
+      left = left.next;
+    } else {
+      currHead.next = right;
+      right = right.next;
     }
 
-    currHead.next = left ? left : right;
-    return resultHead.next;
-  };
+    currHead = currHead.next;
+  }
+
+  currHead.next = left ? left : right;
+  return resultHead.next;
+};
+
+const mergeKLists = (lists) => {
+  if (lists.length === 0) return null;
+  if (lists.length === 1) return lists[0];
+  if (lists.length === 2) return merge(lists[0], lists[1]);
   
   return lists.reduce((acc, curr) => merge(acc, curr), new ListNode(-10002)).next;
 };
