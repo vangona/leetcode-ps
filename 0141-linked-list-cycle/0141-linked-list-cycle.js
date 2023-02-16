067bdf6150577f4953b328ea0ed93478615a80eb
김관경 <69471032+vangona@users.noreply.github.com>
2023-02-17 01:43:51 +0900

    Time: 77 ms (72.78%), Space: 44.6 MB (61.20%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -1,20 +1,12 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-
-/**
- * @param {ListNode} head
- * @return {boolean}
- */
 const hasCycle = (head) => {
-  while (head) {
-    if (head.visited) return true;
-    head.visited = true;
-    head = head.next;
+  if (!head) return false;
+  let slow = head;
+  let fast = head.next;
+  
+  while (fast && fast.next) {
+    if (slow === fast) return true;
+    slow = slow.next;
+    fast = fast.next.next;
   }
   
   return false;
