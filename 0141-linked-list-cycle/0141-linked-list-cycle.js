54b5feb256e081580995559a7bcabe5fa370eb16
김관경 <69471032+vangona@users.noreply.github.com>
2023-02-17 01:19:38 +0900

    Time: 67 ms (96.76%), Space: 44.5 MB (70.10%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,12 +11,11 @@
  * @return {boolean}
  */
 const hasCycle = (head) => {
-  const visited = [];
   let result = false;
   
   while (head && !result) {
-    if (visited.includes(head)) result = true;
-    visited.push(head);
+    if (head.visited) result = true;
+    head.visited = true;
     head = head.next;
   }
   
