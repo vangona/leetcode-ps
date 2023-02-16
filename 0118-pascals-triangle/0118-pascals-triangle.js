0ca06fed75f849dcd3c110d34cb59d5e0227872e
김관경 <69471032+vangona@users.noreply.github.com>
2023-02-17 01:09:20 +0900

    Time: 56 ms (93.56%), Space: 41.5 MB (98.81%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -4,9 +4,8 @@
  */
 const generate = (numRows) => {
   const result = Array.from({length: numRows}, (_, index) => new Array(index + 1).fill(1));
-  result[0] = [1];
   
-  for (let i = 1; i < numRows; i++) {
+  for (let i = 2; i < numRows; i++) {
     const halfNums = Math.ceil((i + 1) / 2);
     for (let j = 1; j < halfNums; j++) {
       const pascalNum = result[i - 1][j - 1] + result[i - 1][j];
