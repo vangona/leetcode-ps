5e8070da3d1388c38f96036dcdd80724d40c2a36
김관경 <69471032+vangona@users.noreply.github.com>
2023-02-17 17:19:27 +0900

    Time: 65 ms (88.46%), Space: 42.9 MB (94.96%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -1,4 +1,21 @@
 const majorityElement = (nums) => {
-	nums.sort((a, b) => a - b);
-	return nums[Math.floor(nums.length / 2)];
+  let n = nums.length;
+  let candidate = nums[0];
+  let count = 0;
+  
+  for (let i = 0; i < n; i++) {
+    if (count === 0) {
+      candidate = nums[i];
+      count = 1;
+      continue;
+    } 
+    
+    if (nums[i] === candidate) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+
+  return candidate;
 };
